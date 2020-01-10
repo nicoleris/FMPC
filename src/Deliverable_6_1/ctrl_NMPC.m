b2537d2af4a9684f2c8efac1fa229c5ac50c0be9
eg10yx <34946305+eg10yx@users.noreply.github.com>
2020-01-10 16:56:41 +0100

    6.1

--- src/Deliverable_6_1/ctrl_NMPC.m
+++ src/Deliverable_6_1/ctrl_NMPC.m
@@ -8,27 +8,22 @@ X = opti.variable(12,N+1); % state trajectory variables
 U = opti.variable(4, N); % control trajectory (thrusts)
 X0 = opti.parameter(12,1); % initial state
 REF = opti.parameter(4,1); % reference position [x,y,z,yaw]
-%%%%%%%%%%%%%%%%%%%%%%%%
-%%%% YOUR CODE HERE %%%%
-%%%%%%%%%%%%%%%%%%%%%%%%
-%epsilon_speed = opti.variable(1,N+1);
+
 f = @(x,u) quad.f(x, u);
 f_discrete = @(x,u) RK4(x,u,h,f);
 
-thrust_parameter=0.6;
+param=[0.7 20 0.5 0.3];
 
 %---- objective ---------
 opti.minimize(...
-  1*(X(10,:) - REF(1))*(X(10,:) - REF(1))' + ... % Go to ref X
-  1*(X(11,:) - REF(2))*(X(11,:) - REF(2))' + ... % Go to ref Y
-  20*(X(12,:) - REF(3))*(X(12,:) - REF(3))' + ... % Go to ref Z
-  0.5*(X(6,:) - REF(4))*(X(6,:) - REF(4))'  + ... % Go to ref Yaw
-  thrust_parameter*U(1,:)*U(1,:)' + ... % Minimize cmd
-  thrust_parameter*U(2,:)*U(2,:)' + ... % Minimize cmd
-  thrust_parameter*U(3,:)*U(3,:)' + ... % Minimize cmd
-  thrust_parameter*U(4,:)*U(4,:)'); % Minimize cmd
-
-
+  param(1)*(X(10,:)-REF(1))*(X(10,:)-REF(1))' + ... % tracking ref X
+  param(1)*(X(11,:)-REF(2))*(X(11,:)-REF(2))' + ... % tracking ref Y
+  param(2)*(X(12,:)-REF(3))*(X(12,:)-REF(3))' + ... % tracking ref Z
+  param(3)*(X(6,:) - REF(4))*(X(6,:)-REF(4))' + ... % tracking ref Yaw
+  param(4)*U(1,:)*U(1,:)' + ... % Minimize input
+  param(4)*U(2,:)*U(2,:)' + ... % Minimize input
+  param(4)*U(3,:)*U(3,:)' + ... % Minimize input
+  param(4)*U(4,:)*U(4,:)');     % Minimize input
 
 for k=1:N % loop over control intervals
   opti.subject_to(X(:,k+1) == f_discrete(X(:,k), U(:,k)));
@@ -36,7 +31,9 @@ end
 
 opti.subject_to(0 <= U <= 1.5);
 opti.subject_to(X(:,1)==X0);
-
+opti.subject_to(X(10,:)<=3.5);
+opti.subject_to(X(11,:)<=1);
+opti.subject_to(X(12,:)<=REF(3));
 
 ctrl = @(x,ref) eval_ctrl(x, ref, opti, X0, REF, X, U);
 end
